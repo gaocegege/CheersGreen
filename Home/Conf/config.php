c9278dfa074c07547ea6fe0a8ca87a3107c968d4
Gaoce <gaocegege@hotmail.com>
2014-12-08 20:44:28 +0800

    add serverDB

--- Home/Conf/config.php
+++ Home/Conf/config.php
@@ -6,10 +6,10 @@ return array(
 	'SHOW_PAGE_TRACE' =>true, // 显示页面Trace信息
 	// 添加数据库配置信息
  	'DB_TYPE'   => 'mysqli', // 数据库类型
- 	'DB_HOST'   => '127.0.0.1', // 服务器地址
- 	'DB_NAME'   => 'db_cheersgreen', // 数据库名
+ 	'DB_HOST'   => '121.40.136.175', // 服务器地址
+ 	'DB_NAME'   => 'cheersgreen', // 数据库名
  	'DB_USER'   => 'root', // 用户名
- 	'DB_PWD'    => '', // 密码
+ 	'DB_PWD'    => 'Zz119911', // 密码
  	'DB_PORT'   => 3306, // 端口
  	'DB_PREFIX' => 'think_', // 数据库表前缀
 );
